17d43104ff181bc0352c02d0ef85111431ff7eb1
ZodiacXxC <123416865+ZodiacXxC@users.noreply.github.com>
2023-10-11 14:21:19 +0300

    Add files via upload

---
+++ testGui.py
@@ -0,0 +1,138 @@
+import customtkinter as ctk
+import tkinter as tk
+import socket
+import threading
+import time
+
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("green")
+
+appWidth, appHeight = 500, 500
+response = ""
+datax = None
+def startCommand():
+    global response
+    command_value = commandOptionMenu.get()
+    process_value = processOptionMenu.get()
+    if command_value == "Kill":
+        process_value = process_value + ".exe"
+        response = f"taskkill /IM {process_value} /F"
+        sendCommandThread = threading.Thread(target=sendCommand)
+        sendCommandThread.start()
+    elif command_value == "Run" :
+        if process_value == "AlSahlHIS" :
+            response = "C:\\Users\\USER\\Desktop\\AlSahlHIS .exe - Shortcut.lnk"
+            sendCommandThread = threading.Thread(target=sendCommand)
+            sendCommandThread.start()
+        else:
+            response = f"start {process_value}"
+            sendCommandThread = threading.Thread(target=sendCommand)
+            sendCommandThread.start()
+    elif command_value == "Shutdown" :
+        response = "shutdown /f /t 30"
+        sendCommandThread = threading.Thread(target=sendCommand)
+        sendCommandThread.start()
+    elif command_value == "Restart" :
+        response = "shutdown /f /r /t 30"
+        sendCommandThread = threading.Thread(target=sendCommand)
+        sendCommandThread.start()
+
+def sendCommand():
+    global response
+    try:
+        sock.send(response.encode("utf-8"))
+    except Exception as e:
+        print("Error sending command:", str(e))
+
+
+
+def changeOptions(select_options):
+    if select_options == "Shutdown" or select_options == "Restart":
+        processOptions = []
+        processOptionMenu.configure(values=processOptions)
+        processOptionMenu.set("")
+    elif select_options == "Kill" or select_options == "Run" :
+        processOptions = ["AlSahlHIS", "Chrome", "Notepad"]
+        processOptionMenu.configure(values=processOptions)
+        processOptionMenu.set("AlSahlHIS")
+
+app = ctk.CTk()
+app.title("K-Server")
+app.geometry(f"{appWidth}x{appHeight}")
+
+programLabel = ctk.CTkLabel(app,text="K-Server",font=("Times New Roman", 36))
+programLabel.grid(row=6, column=1, padx=100, pady=20, sticky="ew")
+
+commandLabel = ctk.CTkLabel(app, text="Command:")
+commandLabel.grid(row=8, column=0, padx=20, pady=20, sticky="ew")
+
+
+commandOptions = ["Kill", "Run", "Shutdown", "Restart"]
+
+commandOptionMenu = ctk.CTkOptionMenu(app ,values=commandOptions,command=changeOptions)
+commandOptionMenu.grid(row=8, column=1, padx=20, pady=20, columnspan=2, sticky="ew")
+
+
+processLabel = ctk.CTkLabel(app, text="Process:")
+processLabel.grid(row=9, column=0, padx=20, pady=20, sticky="ew")
+
+processOptions = ["AlSahlHIS", "Chrome", "Notepad"]
+processOptionMenu = ctk.CTkOptionMenu(app, values=processOptions)
+processOptionMenu.grid(row=9, column=1, padx=20, pady=20, columnspan=2, sticky="ew")
+
+startButton = ctk.CTkButton(app, text="Start a command", command=startCommand)
+startButton.grid(row=10, column=1, columnspan=2, padx=20, pady=20, sticky="ew")
+startButton.configure(state="disabled")
+
+textbox = ctk.CTkTextbox(app,width=280, height=60)
+textbox.grid(row=11, column=1, columnspan=2, padx=20, pady=20, sticky="ew")
+host = "192.160.0.138"
+port = 12345
+
+
+
+def receive(socket, signal):
+    while signal:
+        try:
+            data = socket.recv(32)
+            if not data:
+                break
+            print(data.decode('utf-8'))
+        except:
+            socket.close()
+            TryCon()
+
+def TryCon():
+    global datax
+    while True:
+        try:
+            global sock
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.connect((host, port))
+            textbox.delete(1.0, "end")
+            textbox.insert("end","Server connect...\n")
+            startButton.configure(state="normal")
+            datax = True
+            break
+        except:
+            textbox.insert("end","Server not available. Retrying in 1 second...\n")
+            startButton.configure(state="disabled")
+            datax = False
+            time.sleep(1)
+    StartCon()
+
+def StartCon():
+    receiveThread = threading.Thread(target=receive, args=(sock, True))
+    receiveThread.start()
+    while True:
+        if datax:
+            time.sleep(5)
+            pass
+        else:
+            break
+
+# Create a thread to handle server connections
+server_thread = threading.Thread(target=TryCon)
+server_thread.start()
+
+app.mainloop()
