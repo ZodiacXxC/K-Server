5d90c9cc40df1c43ef3afc0da16d84780b6748e0
ZodiacXxC <123416865+ZodiacXxC@users.noreply.github.com>
2023-10-14 09:01:26 +0300

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,12 +1,16 @@
 import socket
 import threading
-
-
+import datetime
 connections = []
 total_connections = 0
 
 
+x = datetime.datetime.now()
+xdate = str(x.day) + "/" + str(x.month)  + "/" + str(x.year)
+currentTime = ''
 class Client(threading.Thread):
+    x = datetime.datetime.now()
+    xdate = str(x.day) + "/" + str(x.month)  + "/" + str(x.year)
     def __init__(self, socket, address, id, name, signal):
         threading.Thread.__init__(self)
         self.socket = socket
@@ -24,7 +28,9 @@ class Client(threading.Thread):
                 data = self.socket.recv(32)
                 data_en = data.decode('utf-8')
             except:
-                print("Client " + str(self.address) + " has disconnected")
+                currentDateAndTime1 = datetime.datetime.now()
+                currentTime1 = currentDateAndTime1.strftime("%H:%M:%S")
+                print("Client " + str(self.address) + " has disconnected "+ ": "+ currentTime1 + " - " + xdate)
                 self.signal = False
                 connections.remove(self)
                 break
@@ -39,18 +45,19 @@ class Client(threading.Thread):
 
 def newConnections(socket):
     while True:
+        currentDateAndTime = datetime.datetime.now()
+        currentTime = currentDateAndTime.strftime("%H:%M:%S")
         sock, address = socket.accept()
         global total_connections
         connections.append(Client(sock, address, total_connections, "Name", True))
         connections[len(connections) - 1].start()
-        print("New connection at ID " + str(connections[len(connections) - 1]))
+        print("New connection at ID " + str(connections[len(connections) - 1]) + ": "+ currentTime + " - " + xdate)
         total_connections += 1
 
 def main():
     #Get host and port
     host = "0.0.0.0"
     port = 12345
-
     #Create new server socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.bind((host, port))
